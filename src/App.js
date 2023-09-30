a55ae4e92e31d9a335eb9148d1b7a676362db2d4
LasseTN <103989770+LasseTN@users.noreply.github.com>
2023-09-30 13:15:15 +0200

    Pages and Props.children

--- src/App.js
+++ src/App.js
@@ -5,104 +5,15 @@ import { v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
+import Employees from './pages/Employees';
 
 function App() {
-  const [role, setRole] = useState('dev');
-  const [employees, setEmployees] = useState([
-      {
-        id: 1,
-        name: "Bilbo", 
-        role: "Developer", 
-        img: "https://images.pexels.com/photos/2743754/pexels-photo-2743754.jpeg?auto=compress&cs=tinysrgb&w=1600" 
-      },
-      {
-        id: 2,
-        name: "Winther", 
-        role: "Manager", 
-        img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?auto=compress&cs=tinysrgb&w=1600" 
-      },
-      {
-        id: 3,
-        name: "Tim", 
-        role: "Director of Eng.", 
-        img: "https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg?auto=compress&cs=tinysrgb&w=1600" 
-      },
-      {
-        id: 4,
-        name: "John", 
-        role: "Software Engineer", 
-        img: "https://images.pexels.com/photos/14697557/pexels-photo-14697557.jpeg?auto=compress&cs=tinysrgb&w=1600" 
-      },
-      {
-        id: 5,
-        name: "Lawrence", 
-        role: "The DevOps Guy", 
-        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1600" 
-      },
-      {
-        id: 6,
-        name: "Bobby", 
-        role: "Senior", 
-        img: "https://images.pexels.com/photos/2232981/pexels-photo-2232981.jpeg?auto=compress&cs=tinysrgb&w=1600" 
-      },
-    ]);
-
-    function UpdateEmployee(id, newName, newRole) {
-      const updatedEmployees = employees.map((employee) => {
-        if (id == employee.id) {
-          return {...employee, name: newName, role: newRole}
-        }
-        return employee;
-      });
-      setEmployees(updatedEmployees);
-    }
-
-    function newEmployee(name, role, img) {
-      const newEmployee = {
-        id: uuidv4(),
-        name: name,
-        role: role,
-        img: img,
-      }
-      setEmployees([...employees, newEmployee])
-    }
-
-  const showEmployees = true;
   return (
-      <div className="App bg-gray-300 min-h-screen">
-        <Header />
-        {showEmployees ? (
-              <>
-
-                  <div className="flex flex-wrap justify-center my-2">
-                  {employees.map((employee) => {
-                    const editEmployee = (            
-                    <EditEmployee 
-                        id={employee.id}
-                        name={employee.name} 
-                        role={employee.role} 
-                        updateEmployee={UpdateEmployee} 
-                    />
-                  );
-                      return (
-                        <Employee 
-                            key={employee.id}
-                            id={employee.id}
-                            name={employee.name} 
-                            role={employee.role} 
-                            img={employee.img} 
-                            editEmployee={editEmployee}
-                        />
-                    );
-                })}
-                  </div>
-                  <AddEmployee newEmployee={newEmployee} />
-              </>
-          ) : (
-              <p>You cannot see the employees</p>
-          )}
-      </div>
+      <Header>
+          <Employees />
+      </Header>
     );
 }
+
 
 export default App;
