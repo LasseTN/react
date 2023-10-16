77c78ff7d322b185bc2b76a2a36555c362ef6aff
LasseTN <103989770+LasseTN@users.noreply.github.com>
2023-10-16 21:33:30 +0200

    Basic useEffectPractice

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -2,10 +2,19 @@ import { useState, useEffect } from "react";
 
 export default function Dictionary() {
     const [word, setWord] = useState('');
+    const [word2, setWord2] = useState('');
 
     useEffect(() => {
-        console.log('State Updated ', word);
-    });
+        console.log('State Updated ' + word);
+    }, [word]);
+
+    useEffect(() => {
+        console.log('State Updated ' + word2);
+    }, [word2]);
+    
+    //no dependency array --> update for any state change
+    //empty dependency array --> execute once
+    //passing in data --> only execute when those state variables are changed
 
     return (
         <>
@@ -15,7 +24,14 @@ export default function Dictionary() {
                 setWord(e.target.value);
                 }} 
             />
-            <h1>Let´s get the definition for {word}</h1>
+            <h2>Let´s get the definition for {word}</h2>
+            <input 
+                type="text" 
+                onChange={(e) => {
+                setWord2(e.target.value);
+                }} 
+            />
+            <h2>Let´s get the definition for {word2}</h2>
         </>
-    )
+    );
 }
